2546393472383adae6b2f1f852d4b7b33e33e674
Ben Saufley <bensaufley+git@gmail.com>
2017-07-07 17:24:05 -0400

    Use newer reference to DEVTOOLS

--- src/store.js
+++ src/store.js
@@ -23,7 +23,7 @@ export function generateStore(hist, init = {}) {
 
   /* istanbul ignore next */
   if (process.env.NODE_ENV === 'development') {
-    const devToolsExtension = window.devToolsExtension;
+    const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
 
     if (typeof devToolsExtension === 'function') enhancers.push(devToolsExtension());
   }
