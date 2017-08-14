9af3436ff13250089e9e54746ec9d08d25cef640
Ben Saufley <bensaufley+git@gmail.com>
2017-08-13 23:45:49 -0400

    Basic tests for untouched components

---
+++ test/components/PlayerSlot.test.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
+import { wrapContainer } from '../support/container-helper';
+import PlayerDummy from '../dummies/player';
+
+import { PlayerSlot } from '../../src/components/PlayerSlot';
+
+describe('(Container) PlayerSlot', () => {
+  it('renders without exploding', () => {
+    const player = new PlayerDummy(),
+          wrapper = wrapContainer()(PlayerSlot, { player, team: 'a', role: 'decoder' });
+    expect(findRenderedDOMComponentWithClass(wrapper, 'player-label-container')).to.exist;
+  });
+});
---
+++ test/components/Turn.test.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
+import { Card } from 'semantic-ui-react';
+import { wrapContainer } from '../support/container-helper';
+import GameDummy from '../dummies/game';
+
+import TurnContainer from '../../src/components/Turn';
+
+describe('(Container) Turn', () => {
+  let game, initialState;
+
+  beforeEach(() => {
+    game = new GameDummy({ completed: true });
+    initialState = {
+      games: {
+        [game.id]: game.serialize()
+      }
+    };
+  });
+
+  it('renders transmission without exploding', () => {
+    const turn = game.turns.find((t) => t.event === 'transmission'),
+          wrapper = wrapContainer({ initialState })(TurnContainer, { gameId: game.id, ...turn });
+    expect(findRenderedComponentWithType(wrapper, Card)).to.exist;
+  });
+
+  it('renders decoding without exploding', () => {
+    const turn = game.turns.find((t) => t.event === 'decoding'),
+          wrapper = wrapContainer({ initialState })(TurnContainer, { gameId: game.id, ...turn });
+    expect(findRenderedComponentWithType(wrapper, Card)).to.exist;
+  });
+
+  it('renders end without exploding', () => {
+    const turn = game.turns.find((t) => t.event === 'end'),
+          wrapper = wrapContainer({ initialState })(TurnContainer, { gameId: game.id, ...turn });
+    expect(findRenderedComponentWithType(wrapper, Card)).to.exist;
+  });
+});
